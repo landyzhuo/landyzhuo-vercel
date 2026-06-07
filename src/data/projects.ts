bf7826d2bc0f839ddd68903bf23968ebfc1cad44
Jianan (Landy) Zhuo <landy_zhuo@outlook.com>
2026-06-08 03:54:39 +0800

    edited projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Sample Project Placeholder",
     summary: "A random sample project.",
-    tags: [],
+    tags: ["Some", "Sample", "Tags"],
     link: "",
   },
 ] as const;
